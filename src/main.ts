36f4cedab7a1470d1b4ad4972c19206ff15f5c2b
Kento Shirasawa <shirasawa.kento@gmail.com>
2022-03-08 10:46:57 +0900

    feat: typescript

--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,54 @@
-import { app, BrowserWindow, Menu } from 'electron';
+import { app, BrowserWindow, Menu } from "electron";
 
-function createWindow () {
-
+function createWindow() {
   // ウィンドウオプション
   const options: Electron.BrowserWindowConstructorOptions = {
     width: 1024,
     height: 720,
     webPreferences: {
-      nodeIntegration: true
-    }
-  }
+      nodeIntegration: true,
+    },
+  };
 
   // メニューバー
   const template = [
     {
-      label: 'Backlog',
-      submenu: [{
-        label: 'Quit Backlog by Nulab',
-        accelerator: 'Cmd+Q',
-        click: function(){
-          app.quit()
-        }
-      }]
+      label: "Backlog",
+      submenu: [
+        {
+          label: "Quit Backlog by Nulab",
+          accelerator: "Cmd+Q",
+          click: function () {
+            app.quit();
+          },
+        },
+      ],
     },
-  ]
+  ];
 
-  const win = new BrowserWindow(options)
+  const win = new BrowserWindow(options);
   const menuTemp = Menu.buildFromTemplate(template);
-  
+
   Menu.setApplicationMenu(menuTemp);
 
-  win.loadURL('https://novalumo.backlog.com/dashboard')
+  win.loadURL("https://novalumo.backlog.com/dashboard");
 }
 
 // 初期化完了後ウィンドウ生成
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 /* macOS用の設定 ----- */
 
 // Cmd+Qで閉じられるまで終了しない
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
 // Dockアイコンクリック時にウィンドウを再生成
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    createWindow();
   }
-})
-
+});
